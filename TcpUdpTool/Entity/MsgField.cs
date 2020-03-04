dc95610343b2cc3fa05537bf38c4681e9976ab0f
town <wuxuzhen@seaeverit.com>
2020-03-04 09:26:32 +0800

    1  添加TcpUdpTools

---
+++ TcpUdpTool/Entity/MsgField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpUdpTool.Entity
+{
+    public class MsgField
+    {
+        public string Value { get; set; }
+        public int Length { get; set; }
+        public bool Must { get; set; }
+        public string Flag { get; set; }
+        public string Default { get; set; }
+        public string Force { get; set; }
+        public string Brief { get; set; }
+    }
+}
