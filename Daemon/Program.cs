54e7f5077463935772200582e996e2921841a225
town <wuxuzhen@seaeverit.com>
2020-09-30 17:29:39 +0800

    rm xx

---
+++ Daemon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daemon
+{
+    //守护进程
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //待守护进程对象
+            //守护策略
+        }
+    }
+}
