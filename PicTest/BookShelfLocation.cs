937c274d2488e6139a91a071521c880d9885148d
town <wuxuzhen@seaeverit.com>
2021-03-03 11:15:50 +0800

    pic mdf

---
+++ PicTest/BookShelfLocation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicTest
+{
+    /// <summary>
+    /// 书架坐标(相对导航图)
+    /// </summary>
+    public class BookShelfLocation
+    {
+        /// <summary>
+        /// 书架Id
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 书架名称
+        /// </summary>
+        public string BookShelfName { get; set; }
+
+        /// <summary>
+        /// 书架Id
+        /// </summary>
+        public string BookShelfId { get; set; }
+
+        /// <summary>
+        /// 是否A面
+        /// </summary>
+        public bool IsASide { get; set; }
+
+        /// <summary>
+        /// X坐标
+        /// </summary>
+        public int CoordinateX { get; set; }
+
+        /// <summary>
+        /// Y坐标
+        /// </summary>
+        public int CoordinateY { get; set; }
+    }
+}
---
+++ PicTest/DapperUtils.cs
@@ -0,0 +1,140 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PicTest
+{
+    public class DapperUtils
+    {
+        private static string _connString = "server=localhost;port=3306;user id=root;password=123456;database=smartbookshelf_by;";
+        private static object _obj = new object();
+        private static bool _dbConn = false;
+
+        public static string Conn
+        {
+            set
+            {
+                _connString = value;
+            }
+        }
+
+        public static int Execute(string sql, object model)
+        {
+            int rt = 0;
+            try
+            {
+                Monitor.Enter(_obj);
+                using (IDbConnection connection = new MySqlConnection(_connString))
+                {
+                    rt = connection.Execute(sql, model);
+                }
+                return rt;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                throw ex;
+                //LoggerHelper.Error($"MySqlHelper.Execute  sql:{sql}\r\n  {ex.Message}\r\n{ex.StackTrace}");
+                //_dbConn = false;
+                //return rt;
+            }
+            finally
+            {
+                Monitor.Exit(_obj);
+            }
+        }
+
+        public static int Execute(string sql)
+        {
+            int rt = 0;
+            try
+            {
+                Monitor.Enter(_obj);
+                using (IDbConnection connection = new MySqlConnection(_connString))
+                {
+                    rt = connection.Execute(sql);
+                }
+                _dbConn = true;
+                return rt;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                throw ex;
+                //LoggerHelper.Error($"MySqlHelper.Execute  sql:{sql}\r\n  {ex.Message}\r\n{ex.StackTrace}");
+                //_dbConn = false;
+                //return rt;
+            }
+            finally
+            {
+                Monitor.Exit(_obj);
+            }
+        }
+
+        public static List<T> Query<T>(string sql) where T : class
+        {
+            List<T> list = new List<T>();
+            try
+            {
+                Monitor.Enter(_obj);
+                using (IDbConnection connection = new MySqlConnection(_connString))
+                {
+                    list = connection.Query<T>(sql)?.ToList();
+                }
+                _dbConn = true;
+                return list;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                throw ex;
+                //LoggerHelper.Error($"MySqlHelper.Query  sql:{sql}\r\n  {ex.Message}\r\n{ex.StackTrace}");
+                //_dbConn = false;
+                //return list;
+            }
+            //catch (Exception ex)
+            //{
+            //    LoggerHelper.Error($"SqlHelper.Query  sql:{sql}\r\n  {ex.Message}\r\n{ex.StackTrace}");
+            //    return list;
+            //}
+            finally
+            {
+                Monitor.Exit(_obj);
+            }
+        }
+
+        public static T QueryFirst<T>(string sql)
+        {
+            T t = default(T);
+            try
+            {
+                Monitor.Enter(_obj);
+                using (IDbConnection connection = new MySqlConnection(_connString))
+                {
+                    t = connection.QueryFirst<T>(sql);
+                }
+                _dbConn = true;
+                return t;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                throw ex;
+                //LoggerHelper.Error($"MySqlHelper.Query  sql:{sql}\r\n  {ex.Message}\r\n{ex.StackTrace}");
+                //_dbConn = false;
+                //return t;
+            }
+            //catch (Exception ex)
+            //{
+            //    LoggerHelper.Error($"SqlHelper.Query  sql:{sql}\r\n  {ex.Message}\r\n{ex.StackTrace}");
+            //    return list;
+            //}
+            finally
+            {
+                Monitor.Exit(_obj);
+            }
+        }
+    }
+}
